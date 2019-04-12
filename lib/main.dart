e1eaaa011f877e8a20be2ace90909711162e485d
Alvaro Leon Ruiz <alvaro.ruiz@internetstores.de>
2019-04-12 11:28:37 +0200

    NTR | Final code episode 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'dart:async';
+import 'dart:collection';
 
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'src/article.dart';
+import 'package:hn_app/src/article.dart';
+import 'package:hn_app/src/hn_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final hnBloc = HackerNewsBloc();
+  runApp(MyApp(bloc: hnBloc));
+}
 
 class MyApp extends StatelessWidget {
+  final HackerNewsBloc bloc;
+
+  MyApp({
+    Key key,
+    this.bloc,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +26,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Hacker News',
+        bloc: bloc,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  final HackerNewsBloc bloc;
+  MyHomePage({Key key, this.title, this.bloc}) : super(key: key);
 
   final String title;
 
@@ -30,46 +45,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> _ids = [
-    17392995,
-    17397852,
-    17395342,
-    17385291,
-    17387851,
-    17395675,
-    17387438,
-    17393560,
-    17391971,
-    17392455,
-  ];
-
-  Future<Article> _getArticle(int id) async {
-    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
-    final storyRes = await http.get(storyUrl);
-    if (storyRes.statusCode == 200) {
-      return parseArticle(storyRes.body);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: _ids.map((i) =>
-          FutureBuilder<Article>(
-            future: _getArticle(i),
-            builder: (BuildContext context, AsyncSnapshot<Article> snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return _buildItem(snapshot.data);
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            },
-          )
-        ).toList(),
+      body: StreamBuilder<UnmodifiableListView<Article>>(
+          stream: widget.bloc.articles,
+          initialData: UnmodifiableListView<Article>([]),
+          builder: (context, snapshot) => ListView(
+                children: snapshot.data.map(_buildItem).toList(),
+              )),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Top Stories'),
+            icon: Icon(Icons.arrow_drop_up),
+          ),
+          BottomNavigationBarItem(
+            title: Text('New Stories'),
+            icon: Icon(Icons.new_releases),
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0){
+            widget.bloc.storiesType.add(StoriesType.topStories);
+          } else {
+            widget.bloc.storiesType.add(StoriesType.newStories);
+          }
+        },
       ),
     );
   }
@@ -79,7 +85,8 @@ class _MyHomePageState extends State<MyHomePage> {
       key: Key(article.title),
       padding: const EdgeInsets.all(16.0),
       child: ExpansionTile(
-        title: Text(article.title ?? '[null]', style: TextStyle(fontSize: 24.0)),
+        title:
+            Text(article.title ?? '[null]', style: TextStyle(fontSize: 24.0)),
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
---
+++ lib/src/hn_bloc.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:hn_app/src/article.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+
+enum StoriesType {
+  topStories,
+  newStories
+}
+
+class HackerNewsBloc {
+  final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
+  
+  var _articles = <Article>[];
+
+  StreamSink get storiesType => _storiesTypeController.sink;
+  final _storiesTypeController = StreamController<StoriesType>();  
+
+  static List<int> _newIds = [
+    17395675,
+    17387438,
+    17393560,
+    17391971,
+    17392455,
+  ];
+
+  static List<int> _topIds = [
+    17392995,
+    17397852,
+    17395342,
+    17385291,
+    17387851,
+  ];
+
+  HackerNewsBloc() {
+    _getArticlesAndUpdate(_topIds);
+    _storiesTypeController.stream.listen((storiesType) {
+      if (storiesType == StoriesType.newStories) {
+        _getArticlesAndUpdate(_newIds);
+      } else {
+        _getArticlesAndUpdate(_topIds);
+      }
+    });
+  }
+
+  _getArticlesAndUpdate(List<int> ids) {
+    _updateArticles(ids).then((_) {
+      _articlesSubject.add(UnmodifiableListView(_articles));
+    });
+  }
+
+  Stream<UnmodifiableListView<Article>> get articles => _articlesSubject.stream;
+
+  Future<Null> _updateArticles(List<int> articleIds) async {
+    final futureArticles = articleIds.map((id) => _getArticle(id));
+    final articles = await Future.wait(futureArticles);
+    _articles = articles;
+  }
+
+  Future<Article> _getArticle(int id) async {
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+  }
+}
